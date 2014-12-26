d7fe66fb3b36361c4f9fe080296ff93036ee4169
Tim Heuett <tim.heuett@gmail.com>
2014-12-25 19:24:25 -0800

    changed to sinatra dir structure

--- config.rb
+++ config.rb
@@ -3,13 +3,15 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "/public/css"
+sass_dir = "/sass"
+images_dir = "/public/img"
+javascripts_dir = "/public/js"
+fonts_dir = "/public/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
