eca83a076deb6805948fd2871c5bfc545b45f8ab
Tim Heuett <tim.heuett@gmail.com>
2014-12-29 04:35:21 -0800

    modularized app

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
+require 'sinatra/base'
 require 'haml'
 
-enable :run
+module MyApp
+	class Application < Sinatra::Base
+		enable :run
 
-get '/' do
-	haml :index
-end
+		get '/' do
+			haml :index
+		end
 
-helpers do
-	def link_to href, text, attributes = {}
-		attr_string = attributes.map{ |k,v| "#{k.to_s}=\"#{v.to_s}\""}.join(" ")
-		return "<a href=\"#{href}\" #{attr_string}>#{text}</a>"
+		helpers do
+			def link_to href, text, attributes = {}
+				attr_string = attributes.map{ |k,v| "#{k.to_s}=\"#{v.to_s}\""}.join(" ")
+				return "<a href=\"#{href}\" #{attr_string}>#{text}</a>"
+			end
+		end
+
 	end
 end
