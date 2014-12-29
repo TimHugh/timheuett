f026bba643cc0f0b14d5700233816233b21cc969
Tim Heuett <tim.heuett@gmail.com>
2014-12-29 03:56:34 -0800

    added link helper

--- app.rb
+++ app.rb
@@ -9,3 +9,9 @@ get '/' do
 	erb :index
 end
 
+helpers do
+	def link_to href, text, attributes = {}
+		attr_string = attributes.map{ |k,v| "#{k.to_s}=\"#{v.to_s}\""}.join(" ")
+		return "<a href=\"#{href}\" #{attr_string}>#{text}</a>"
+	end
+end
