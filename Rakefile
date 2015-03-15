76fbe004ad00d88e5318838378cae16cdbd1160f
Tim Heuett <tim.heuett@gmail.com>
2015-03-15 00:10:12 -0700

    code style changes

--- Rakefile
+++ Rakefile
@@ -1,37 +1,44 @@
+task default: 'test'
+
 # setup logging
 require 'logger'
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
 
+# run tests
+require 'rake/testtask'
+Rake::TestTask.new do |t|
+  t.libs << "spec"
+  t.pattern = "spec/**/*_spec.rb"
+end
 
 # console task
 desc "Start a console inside the app environment"
 task :console do
-	require 'irb'
-	require 'irb/completion'
-	require './app.rb'
+  require 'irb'
+  require 'irb/completion'
+  require './app.rb'
 
-	ARGV.clear
-	IRB.start
+  ARGV.clear
+  IRB.start
 end
-
 
 # keepalive for heroku server
 desc "Ping server to keep dyno alive"
 task :keepalive do
-	require "net/http"
+  require "net/http"
 
-	logger.info "Running rake task :keepalive."
+  logger.info "Running rake task :keepalive."
 
-	if ENV['PING_URL']
-		logger.info "\tPinging #{ENV['PING_URL']}."
-		uri = URI(ENV['PING_URL'])
-		if Net::HTTP.get_response(uri)
-			logger.info "\t\tSuccess!"
-		else
-			logger.info "\t\tNo response."
-		end
-	else
-		logger.warn "\tENV['PING_URL'] is not set."
-	end
+  if ENV['PING_URL']
+    logger.info "\tPinging #{ENV['PING_URL']}."
+    uri = URI(ENV['PING_URL'])
+    if Net::HTTP.get_response(uri)
+      logger.info "\t\tSuccess!"
+    else
+      logger.info "\t\tNo response."
+    end
+  else
+    logger.warn "\tENV['PING_URL'] is not set."
+  end
 end
--- app.rb
+++ app.rb
@@ -4,35 +4,28 @@ require 'sinatra/base'
 require 'haml'
 
 module MyApp
-	class Application < Sinatra::Base
+  class Application < Sinatra::Base
+    index = lambda do
+      haml :index
+    end
 
-		index = lambda do
-			haml :index
-		end
+    fourohfour = lambda do
+      status 404
+      haml :fourohfour
+    end
 
-		fourohfour = lambda do
-			status 404
-			haml :fourohfour
-		end
+    # routes
+    get '/', &index
+    get '/*', &index
 
-
-		# routes
-		get '/', &index
-		get '/*', &index
-
-
-		helpers do
-			# link_to helper
-			#		Creates a link element like so: <a>text</a>
-			#		The optional attributes hash accepts "stringable" types for both key and hash
-			def link_to text, attributes = {}
-				# parse attributes (if there are any) into a string
-				attr_string = attributes.map{ |k,v| "#{k.to_s}=\"#{v.to_s}\""}.join(" ")
-				# put HTML string together and return
-				return "<a #{attr_string}>#{text}</a>"
-			end
-
-		end
-
-	end
+    helpers do
+      # link_to helper
+      #    Creates a link element like so: <a attr="val">text</a>
+      #    The optional attributes hash accepts "stringable" types for both key and hash
+      def link_to(text, attributes = {})
+        attr_string = attributes.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+        "<a #{attr_string}>#{text}</a>"
+      end
+    end
+  end
 end
