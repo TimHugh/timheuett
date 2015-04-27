015e88f0a3c33ae311164eec0c2133592ca6c6d3
Tim Heuett <tim.heuett@gmail.com>
2015-04-26 18:47:18 -0700

    add redirect test

--- test/features/root_test.rb
+++ test/features/root_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class RootTest < Test
-  def test_root_loads_and_has_content
+  def test_root_load_and_has_content
     get '/'
     assert last_response.ok?
     assert_match /Tim Heuett/, last_response.body
     assert_match /info@timheuett.com/, last_response.body
+  end
+
+  def test_miss_redirects_to_root
+    get '/whatever'
+    assert last_response.redirect?
+    assert_equal 'http://example.org/', last_response.location
   end
 end
