343c58e773a66675c594a60af3674c33396bcff2
Tim Heuett <tim.heuett@gmail.com>
2016-05-15 16:16:13 -0700

    integrate fonts into asset pipeline

--- config/assets.rb
+++ config/assets.rb
@@ -2,9 +2,12 @@ module Site
   module Assets
     def self.environment root_path
       @environment ||= (Sprockets::Environment.new root_path).tap do |env|
+        env.append_path 'assets/fonts'
         env.append_path 'assets/javascripts'
+
         env.css_compressor = :scss
         env.append_path 'assets/stylesheets'
+
         AutoprefixerRails.install(env)
       end
     end
